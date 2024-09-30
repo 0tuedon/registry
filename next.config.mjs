5d2f624e233dde6ca1d10247390853ff3b6c2a5e
0tuedon <90271995+0tuedon@users.noreply.github.com>
2024-09-30 10:12:17 +0100

    fix: moved all /transcripts to /categories

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
         },
         {
           source: "/transcripts",
-          destination: "/categories",
+          destination: "/gh-pages/index.html",
         },
         {
           source: "/:path*",
